308e42f2031487eeb59923f737f64a68f0fd45ca
Vikas Saini <154893113+Vikas-Saini21@users.noreply.github.com>
2024-09-18 15:54:25 +0530

    Add files via upload

---
+++ type_conversion.php
@@ -0,0 +1,29 @@
+<!-- Type Conversion in php -->
+<!-- int into string -->
+<?php
+$x=10;
+$y=(string)$x;
+var_dump($y);
+?>
+
+<!--int into float-->
+<?php
+$x=10;
+$y=(float)$x;
+var_dump($y);
+?>
+
+<!-- int into array -->
+<?php
+$x=10;
+$y=(array)$x;
+var_dump($y);
+?>
+
+<!-- int into boolean -->
+<?php
+$x=10;
+$y=(bool)$x;
+var_dump($y);
+?>
+
